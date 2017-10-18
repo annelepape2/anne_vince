3a17089bd8a76c0b83e00ac83867c63bae54b08a
LE PAPE Anne-Hélène <annelepape2@gmail.com>
2017-10-18 18:30:51 +0200

    vendor

--- ttp/routes/web.php
+++ ttp/routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('image-upload',['as'=>'image.upload','uses'=>'ImageUploadController@imageUpload']);
+Route::post('image-upload',['as'=>'image.upload.post','uses'=>'ImageUploadController@imageUploadPost']);
