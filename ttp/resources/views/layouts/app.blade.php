9b2bd3f345fcdd07ea40ccd527b16da61758dae6
quentin78 <quentin29norm@gmail.com>
2017-10-21 23:46:44 +0200

    refonte page squelette pages home / welcome  / app.blade

--- ttp/resources/views/layouts/app.blade.php
+++ ttp/resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ URL::asset('/public/css/app.css') }}" rel="stylesheet">
+
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -11,61 +13,59 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link href="/css/app.css" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
+        <nav class="navbar navbar-default navbar-static-top" style="background:#e74c3c;margin-bottom:0px;!important;border:none; ">
             <div class="container">
                 <div class="navbar-header">
-
                     <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                            data-target="#app-navbar-collapse">
                         <span class="sr-only">Toggle Navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-
                     <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
+                    <a href="#"><img
+                                    src="https://icon-icons.com/icons2/537/PNG/512/camera_icon-icons.com_52946.png" class="img-responsive"
+                                    style="height:40px;width:80px;margin-top:5px;"></a></span>
                 </div>
-
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @if (Auth::guest())
+                            <li><a href="{{ route('login') }}">Se connecter</a></li>
+                            <li><a href="{{ route('register') }}">Inscription</a></li>
                         @else
+                            <li><a href="{{ url('/') }}">Accueil</a></li>
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                   aria-expanded="false" style="position:relative; padding-left: 50px;">
+                                    <img src="/uploads/avatars/{{ Auth::user()->avatar }}"
+                                         style="width: 32px; height: 32px; border-radius: 50%; position: absolute; top: 10px; left: 10px;">
+                                    {{ Auth::user()->first_name }} <span class="caret"></span>
                                 </a>
-
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
                                         <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
+                                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();"><i
+                                                    class="fa fa-btn fa-sign-out"></i> Deconnexion</a>
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                              style="display: none;">
                                             {{ csrf_field() }}
                                         </form>
                                     </li>
                                 </ul>
                             </li>
-                        @endguest
+                        @endif
                     </ul>
                 </div>
             </div>
@@ -78,3 +78,11 @@
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
+
+<style>
+
+    .navbar-default .navbar-nav > li > a {
+        color:#ffffff; !important;
+    }
+
+</style>
--- ttp/resources/views/welcome.blade.php
+++ ttp/resources/views/welcome.blade.php
@@ -1,3 +1,14 @@
-@include('welcome.head')
-@include('welcome.body')
-</html>
+@extends('layouts.app')
+@section('content')
+
+    <div class="section-welcome">
+
+    <h1 class="h1welcome">
+        Titre
+    </h1>
+
+        <p> lorem ipsum dqsdsq</p>
+    </div>
+
+
+@endsection
